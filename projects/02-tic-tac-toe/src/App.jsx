98b846f99c20637c78c93d269541703e37b851a5
SantiagoSalcedoForero <santiagosalcedo.personal@gmail.com>
2023-05-12 08:20:57 -0500

    componetizacion avanzada

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -3,25 +3,32 @@ import confetti from "canvas-confetti"
 
 import { Square } from './components/Square.jsx'
 import { TURNS } from './constants.js'
-import { checkWinnerFrom } from './logic/board.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
-  const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
 
   const updateBoard = (index) => {
 
@@ -33,7 +40,11 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
-
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+    
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti()
--- projects/02-tic-tac-toe/src/logic/board.js
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -12,3 +12,7 @@ export const checkWinnerFrom = (boardToCheck) => {
     }
     return null
 }
+
+export const checkEndGame = (newBoard) => {
+  return newBoard.every((square) => square !== null)
+}
